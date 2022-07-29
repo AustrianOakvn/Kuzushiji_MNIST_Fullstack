2b7ae0c9f567c39113d5e53333e0c325754b38e2
AustrianOakvn <austrian.oak.vn@gmail.com>
2022-07-29 15:11:35 +0000

    add comment for better understand

--- Modeling/DL_pytorch_hydra/srcs/logger.py
+++ Modeling/DL_pytorch_hydra/srcs/logger.py
@@ -7,6 +7,9 @@ from srcs.utils import get_logger
 
 
 class TensorboardWriter():
+    """
+    Tensorboard:
+    """
     def __init__(self, log_dir, enabled):
         self.logger = get_logger('tensorboard-writer')
         self.writer = SummaryWriter(log_dir) if enabled else None
@@ -24,6 +27,7 @@ class TensorboardWriter():
         self.timer = datetime.now()
 
     def set_step(self, step):
+        """"""
         self.step = step
         if step == 0:
             self.timer = datetime.now()
@@ -52,6 +56,8 @@ class TensorboardWriter():
 
 
 class BatchMetrics:
+    """
+    """
     def __init__(self, *keys, postfix='', writer=None):
         self.writer = writer
         self.postfix = postfix
@@ -61,10 +67,14 @@ class BatchMetrics:
         self.reset()
 
     def reset(self):
+        """
+        """
         for col in self._data.columns:
             self._data[col].values[:] = 0
 
     def update(self, key, value, n=1):
+        """
+        """
         if self.postfix:
             key = key + self.postfix
         if self.writer is not None:
@@ -74,27 +84,44 @@ class BatchMetrics:
         self._data.average[key] = self._data.total[key] / self._data.counts[key]
 
     def avg(self, key):
+        """
+        """
         if self.postfix:
             key = key + self.postfix
         return self._data.average[key]
 
     def result(self):
+        """
+        """
         return dict(self._data.average)
 
 
 class EpochMetrics:
+    """
+    Instances: logger, data (dataframe to store metrics), topk_idx (save best epoch idx)
+    """
     def __init__(self, metric_names, phases=('train', 'valid'), monitoring='off'):
+        """
+        Parameters:
+        - metric names: name of all the metrics
+        """
         self.logger = get_logger('epoch-metrics')
         # setup pandas DataFrame with hierarchical columns
+        # product function return cartesian product, which is all the possible combination
         columns = tuple(product(metric_names, phases))
         self._data = pd.DataFrame(columns=columns)  # TODO: add epoch duration
         self.monitor_mode, self.monitor_metric = self._parse_monitoring_mode(monitoring)
         self.topk_idx = []
 
     def minimizing_metric(self, idx):
+        """
+        Get the metric of one epoch from data 
+        If monitor_mode is min then set the same metric. If monitor_mode is max then add the subtraction before metric
+        """
         if self.monitor_mode == 'off':
             return 0
         try:
+            # Get metric as a row from the dataframe by access its index
             metric = self._data[self.monitor_metric].loc[idx]
         except KeyError:
             self.logger.warning("Warning: Metric '{}' is not found. "
@@ -107,6 +134,9 @@ class EpochMetrics:
             return - metric
 
     def _parse_monitoring_mode(self, monitor_mode):
+        """
+        Parameters: monitor_mode consist of monitor_mode and monitor_metric. By default is set "off"
+        """
         if monitor_mode == 'off':
             return 'off', None
         else:
@@ -116,6 +146,10 @@ class EpochMetrics:
         return monitor_mode, monitor_metric
 
     def is_improved(self):
+        """
+        Get the index of last epoch and the index of best epoch and compare if they are the same
+        If true: is improved; If false: no improved
+        """
         if self.monitor_mode == 'off':
             return True
 
@@ -145,6 +179,11 @@ class EpochMetrics:
                 pass
 
     def update(self, epoch, result):
+        """
+        Update result to the metric dataframe with row name is the epoch index
+        Save epoch index to topk_idx
+        Sort the topk_idx according to the metrics
+        """
         # print(type(result))
         # print(result.items())
         epoch_idx = f'epoch-{epoch}'
@@ -154,10 +193,19 @@ class EpochMetrics:
         self.topk_idx = sorted(self.topk_idx, key=self.minimizing_metric)
 
     def latest(self):
+        """
+        Get the last element in the dataframe
+        """
         return self._data[-1:]
 
     def to_csv(self, save_path=None):
+        """
+        Log metric data to csv file
+        """
         self._data.to_csv(save_path)
 
     def __str__(self):
+        """
+        Return the string representation of metric data
+        """
         return str(self._data)
